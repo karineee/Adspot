b33c29f62050b54e54fa217debaef0be23c8f81a
karineee <iwamura.karine@gmail.com>
2019-07-30 17:25:36 -0700

    Customer Recommendations

--- ConsumerHomeActivity.java
+++ ConsumerHomeActivity.java
@@ -208,7 +208,7 @@ public class ConsumerHomeActivity extends AppCompatActivity {
             protected String doInBackground(Void... params) {
                 String Result = "0";
 
-                Log.d("myTag", "Hello future Karine! ");
+
                 try {
                     res = mTable
                             .select("username")
@@ -222,9 +222,8 @@ public class ConsumerHomeActivity extends AppCompatActivity {
                         Result = obj.getUsername();
                         Log.d("TASKREPLY0: ", Result);
 
-                        Log.d("myTag", "Hello future Karine! ");
                         if (Result.equals("Testman1")) {
-                            Log.d("myTag", "Hello future Karine! ");
+
                             break;
                         }
                         i = i + 1;
@@ -264,6 +263,26 @@ public class ConsumerHomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_consumer_home);
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        String username = message.substring(0, message.indexOf('.'));
+        String password = message.substring((message.indexOf('.') + 1));
+
+        //Welcome user message
+
+        master = "Welcome, " + username + "!";
+
+
+
+
+        //***If a Consumer user found, then provide recommendations for Consumer user. ***
+
+        if (username.equals("Consumer")) {
+        //Start
+
         try {
             con = connectionClass();
             if (con == null) {
@@ -274,15 +293,6 @@ public class ConsumerHomeActivity extends AppCompatActivity {
                 String query = "select * from RecommendationTable";
                 Statement stmt = con.createStatement();
                 ResultSet rs = stmt.executeQuery(query);
-
-
-
-//change
-                //test search by id from
-
-
-
-
 
 
 
@@ -332,6 +342,71 @@ public class ConsumerHomeActivity extends AppCompatActivity {
                         Log.d("myTag", "Rec5: " + name9);
                         Log.d("myTag", "Link5: " + name10);
 
+
+
+
+
+
+
+                        // Capture the layout's TextView and set the string as its text from the res/layout/activity_consumer_home.xml
+
+
+
+                        ImageView ListImg2 = findViewById(R.id.ListImg2);
+                        ImageView ListImg3 = findViewById(R.id.ListImg3);
+                        ImageView ListImg4 = findViewById(R.id.ListImg4);
+                        // ImageView ListImg5 = findViewById(R.id.ListImg5);
+
+
+
+
+
+                        TextView textView = findViewById(R.id.textView2);
+                        textView.setText(master);
+
+                        //Displays product 2 image
+                        ImageView ListPic2 = findViewById(R.id.ListImg1);
+                        Picasso.get().load(name5).into(ListPic2);
+
+
+                        //Displays produce 2 name
+                        master = name2;
+                        TextView textView3 = findViewById(R.id.ListTxt1);
+                        textView3.setText(master);
+
+
+                        //Displays product 3 image
+                        ImageView ListPic3 = findViewById(R.id.ListImg2);
+                        Picasso.get().load(name6).into(ListPic3);
+
+                        //Displays product 3 name
+                        master = name3;
+                        TextView textView4 = findViewById(R.id.ListTxt2);
+                        textView4.setText(master);
+
+
+                        //Displays product 4 image
+                        ImageView ListPic4 = findViewById(R.id.ListImg3);
+                        Picasso.get().load(name8).into(ListPic4);
+
+                        //Displays product 4 name
+                        master = name7;
+                        TextView textView5 = findViewById(R.id.ListTxt3);
+                        textView5.setText(master);
+
+                        //Displays product 5 image
+                        ImageView ListPic5 = findViewById(R.id.ListImg4);
+                        Picasso.get().load(name10).into(ListPic5);
+
+                        //Displays product 5 name
+                        master = name9;
+                        TextView textView6 = findViewById(R.id.ListTxt4);
+                        textView6.setText(master);
+
+
+
+
+
                     }
                     rs.next();
                 }
@@ -339,27 +414,12 @@ public class ConsumerHomeActivity extends AppCompatActivity {
 
                 if (rs.next()) {
 
-                  /*
-                    name1 = rs.getString("Rec1"); // Name of Column
-                    name2 = rs.getString("Rec2"); // Name of Column
-                    name3 = rs.getString("Rec3"); // Name of Column
-                    name4 = rs.getString("Link1"); // Name of Column
-                    name5 = rs.getString("Link2"); // Name of Column
-                    name6 = rs.getString("Link3"); // Name of Column */
-
-                    /* OUTPUTS SECOND PRODUCT IN LIST... rs.next() goes down a row!
-                    if (rs.next()){
-                        name1 = rs.getString("Rec1");
-                        Log.d("myTag", "Hello future Karine! " + name1);
-
-                    }*/
-
                     z = "query successful";
 
                     isSuccess = true;
                     if (isSuccess) {
-                        message = (TextView) findViewById(R.id.textView2);
-                        message.setText(name1);
+                        //message = (TextView) findViewById(R.id.textView2);
+                      //  message.setText(name1);
                     }
                     con.close();
 
@@ -374,24 +434,6 @@ public class ConsumerHomeActivity extends AppCompatActivity {
 
             Log.d("sql error", z);
         }
-        // return z;
-
-
-
-        // Get the Intent that started this activity and extract the string
-        Intent intent = getIntent();
-        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-
-        String username = message.substring(0, message.indexOf('.'));
-        String password = message.substring((message.indexOf('.') + 1));
-
-
-//changed output
-
-        // String master = name1;
-        // String master = z;
-        // String master = "welcome";
-        // String master = message;
 
 
 
@@ -399,86 +441,16 @@ public class ConsumerHomeActivity extends AppCompatActivity {
 
 
 
+        } /* else if (username.equals("MrHappy")) {
+            ImageView product1 = findViewById(R.id.ListImg1);
+            product1.setImageResource(R.drawable.sad);
 
+            ImageView product2 = findViewById(R.id.ListImg2);
+            product2.setImageResource(R.drawable.sad);
 
-
-        // Capture the layout's TextView and set the string as its text from the res/layout/activity_consumer_home.xml
-
-
-
-        ImageView ListImg2 = findViewById(R.id.ListImg2);
-        ImageView ListImg3 = findViewById(R.id.ListImg3);
-        ImageView ListImg4 = findViewById(R.id.ListImg4);
-        ImageView ListImg5 = findViewById(R.id.ListImg5);
-
-
-
-
-        //Welcome user message
-
-        master = "Welcome, " + username + "!";
-
-        TextView textView = findViewById(R.id.textView2);
-        textView.setText(master);
-
-
-        //Displays product 1 image
-        ImageView ListPic1 = findViewById(R.id.ListImg1);
-        //Picasso.get().load("http://i.imgur.com/DvpvklR.png").into(ListPic1);
-        Picasso.get().load(name4).into(ListPic1);
-
-        //Displays product 1 name
-        master = name1;
-        TextView textView2 = findViewById(R.id.ListTxt1);
-        textView2.setText(master);
-
-        //Displays product 2 image
-        ImageView ListPic2 = findViewById(R.id.ListImg2);
-        Picasso.get().load(name5).into(ListPic2);
-
-
-        //Displays produce 2 name
-        master = name2;
-        TextView textView3 = findViewById(R.id.ListTxt2);
-        textView3.setText(master);
-
-
-        //Displays product 3 image
-        ImageView ListPic3 = findViewById(R.id.ListImg3);
-        Picasso.get().load(name6).into(ListPic3);
-
-        //Displays product 3 name
-        master = name3;
-        TextView textView4 = findViewById(R.id.ListTxt3);
-        textView4.setText(master);
-
-
-        //CONT ...
-
-        //Displays product 4 image
-        ImageView ListPic4 = findViewById(R.id.ListImg4);
-        Picasso.get().load(name8).into(ListPic4);
-
-        //Displays product 4 name
-        master = name7;
-        TextView textView5 = findViewById(R.id.ListTxt4);
-        textView5.setText(master);
-
-        //Displays product 5 image
-        ImageView ListPic5 = findViewById(R.id.ListImg5);
-        Picasso.get().load(name10).into(ListPic5);
-
-        //Displays product 5 name
-        master = name9;
-        TextView textView6 = findViewById(R.id.ListTxt5);
-        textView6.setText(master);
-
-
-
-
-
-        if (username.equals("MrHappy")) {
-
+            ImageView product3 = findViewById(R.id.ListImg3);
+            product3.setImageResource(R.drawable.sad);
+        } else if (username.equals("MrSad")) {
             ImageView product1 = findViewById(R.id.ListImg1);
             product1.setImageResource(R.drawable.happy);
 
@@ -491,17 +463,231 @@ public class ConsumerHomeActivity extends AppCompatActivity {
             ImageView product4 = findViewById(R.id.ListImg4);
             product4.setImageResource(R.drawable.happy);
 
-            ImageView product5 = findViewById(R.id.ListImg5);
-            product5.setImageResource(R.drawable.happy);
-        } else if (username.equals("MrSad")) {
-            ImageView product1 = findViewById(R.id.ListImg1);
+
+        }*/
+
+
+        //***If the AzureDude user is found, then provide recommendations for AzureDude user. ***
+         else if (username.equals("AzureDude")) {
+           /* ImageView product1 = findViewById(R.id.ListImg1);
             product1.setImageResource(R.drawable.sad);
+*/
 
-            ImageView product2 = findViewById(R.id.ListImg2);
-            product2.setImageResource(R.drawable.sad);
 
-            ImageView product3 = findViewById(R.id.ListImg3);
-            product3.setImageResource(R.drawable.sad);
+
+
+
+
+
+
+
+
+
+
+            try {
+                con = connectionClass();
+                if (con == null) {
+                    z = "Check your Internet Access!";
+
+                } else {
+                    //Change below query according to your database
+                    String query = "select * from RecommendationTable";
+                    Statement stmt = con.createStatement();
+                    ResultSet rs = stmt.executeQuery(query);
+
+
+
+//change
+                    //test search by id from
+
+
+
+
+
+
+
+
+
+                    while(rs.next()){
+                        name =  rs.getString("Id"); // Name of Column
+                        Log.d("myTag", "Id: " + name);
+
+
+                        if( name.equals("1"))
+                        {
+                            Log.d("myTag", "Id found: " + name);
+
+
+                        /* BEFORE
+                        name1 = rs.getString("Rec1"); // Name of Column
+                        name2 = rs.getString("Rec2"); // Name of Column
+                        name3 = rs.getString("Rec3"); // Name of Column
+                        name4 = rs.getString("Link1"); // Name of Column
+                        name5 = rs.getString("Link2"); // Name of Column
+                        name6 = rs.getString("Link3"); // Name of Column
+                            */
+
+                            //AFTER
+                            name1 = rs.getString("Rec3"); // Name of Column
+                            name2 = rs.getString("Rec2"); // Name of Column
+                            name3 = rs.getString("Rec1"); // Name of Column
+                            name4 = rs.getString("Link3"); // Name of Column
+                            name5 = rs.getString("Link2"); // Name of Column
+                            name6 = rs.getString("Link1"); // Name of Column
+                            name7 = rs.getString("Rec4"); // Name of Column
+                            name8 = rs.getString("Link4"); // Name of Column
+                            name9 = rs.getString("Rec5"); // Name of Column
+                            name10 = rs.getString("Link5"); // Name of Column
+
+
+
+                            Log.d("myTag", "Product1: " + name1);
+                            Log.d("myTag", "Link1: " + name4);
+                            Log.d("myTag", "Product2: " + name2);
+                            Log.d("myTag", "Link2: " + name5);
+                            Log.d("myTag", "Product3: " + name3);
+                            Log.d("myTag", "Link3: " + name6);
+                            Log.d("myTag", "Rec4: " + name7);
+                            Log.d("myTag", "Link4: " + name8);
+                            Log.d("myTag", "Rec5: " + name9);
+                            Log.d("myTag", "Link5: " + name10);
+
+
+
+
+
+//changed output
+
+                            // String master = name1;
+                            // String master = z;
+                            // String master = "welcome";
+                            // String master = message;
+
+
+
+
+
+
+
+
+
+
+                            // Capture the layout's TextView and set the string as its text from the res/layout/activity_consumer_home.xml
+
+
+
+                            ImageView ListImg2 = findViewById(R.id.ListImg2);
+                            ImageView ListImg3 = findViewById(R.id.ListImg3);
+                            ImageView ListImg4 = findViewById(R.id.ListImg4);
+                            // ImageView ListImg5 = findViewById(R.id.ListImg5);
+
+
+
+
+
+                            TextView textView = findViewById(R.id.textView2);
+                            textView.setText(master);
+            /*
+
+        //Displays product 1 image
+        ImageView ListPic1 = findViewById(R.id.ListImg1);
+        //Picasso.get().load("http://i.imgur.com/DvpvklR.png").into(ListPic1);
+        Picasso.get().load(name4).into(ListPic1);
+
+        //Displays product 1 name
+        master = name1;
+        TextView textView2 = findViewById(R.id.ListTxt1);
+        textView2.setText(master);
+            */
+                            //Displays product 2 image
+                            ImageView ListPic2 = findViewById(R.id.ListImg1);
+                            Picasso.get().load(name5).into(ListPic2);
+
+
+                            //Displays produce 2 name
+                            master = name2;
+                            TextView textView3 = findViewById(R.id.ListTxt1);
+                            textView3.setText(master);
+
+
+                            //Displays product 3 image
+                            ImageView ListPic3 = findViewById(R.id.ListImg2);
+                            Picasso.get().load(name6).into(ListPic3);
+
+                            //Displays product 3 name
+                            master = name3;
+                            TextView textView4 = findViewById(R.id.ListTxt2);
+                            textView4.setText(master);
+
+
+                            //Displays product 4 image
+                            ImageView ListPic4 = findViewById(R.id.ListImg3);
+                            Picasso.get().load(name8).into(ListPic4);
+
+                            //Displays product 4 name
+                            master = name7;
+                            TextView textView5 = findViewById(R.id.ListTxt3);
+                            textView5.setText(master);
+
+                            //Displays product 5 image
+                            ImageView ListPic5 = findViewById(R.id.ListImg4);
+                            Picasso.get().load(name10).into(ListPic5);
+
+                            //Displays product 5 name
+                            master = name9;
+                            TextView textView6 = findViewById(R.id.ListTxt4);
+                            textView6.setText(master);
+
+
+
+
+
+                        }
+                        rs.next();
+                    }
+
+
+                    if (rs.next()) {
+
+                        z = "query successful";
+
+                        isSuccess = true;
+                        if (isSuccess) {
+                            //message = (TextView) findViewById(R.id.textView2);
+                            //  message.setText(name1);
+                        }
+                        con.close();
+
+                    } else {
+                        z = "Invalid query";
+                        isSuccess = false;
+                    }
+                }
+            } catch (Exception ex) {
+                isSuccess = false;
+                z = ex.getMessage();
+
+                Log.d("sql error", z);
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
 
 
